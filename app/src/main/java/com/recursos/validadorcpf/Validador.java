881ff0a6478dab8a4e2d4ad31950edc75d710fc8
Lucas Môra <mora.lucas@live.com>
2020-11-13 04:40:51 -0300

    Pronta classe Validador.
    - Alterado ícone do app e launcher.

--- app/src/main/java/com/recursos/validadorcpf/Validador.java
+++ app/src/main/java/com/recursos/validadorcpf/Validador.java
@@ -1,15 +1,62 @@
 package com.recursos.validadorcpf;
 
+import android.util.Log;
+
+import java.lang.String;
+
 public class Validador {
+    public static boolean isValid(String cpf) {
+        // Verifica se os números foram preenchidos
+        if (cpf.length() != 11) {
+            Log.println(Log.INFO, "Anotação: ", "Não foram preenchidos todos os números.");
+            return false;
+        }
 
-    public static void main(String[] args) {
-        String meucpf = "08308808956";
-        System.out.println(cpfValido(meucpf));
+        // Verifica se os números não são todos iguais
+        if (cpf.equals("11111111111") || cpf.equals("22222222222") || cpf.equals("33333333333")
+                || cpf.equals("44444444444") || cpf.equals("55555555555") || cpf.equals("66666666666")
+                || cpf.equals("77777777777") || cpf.equals("88888888888") || cpf.equals("99999999999")) {
+            Log.println(Log.INFO, "Anotação: ", "Foram preenchidos números iguais.");
+            return false;
+        }
+
+        int i = 0;
+        char atual = cpf.charAt(i);
+        char proximo = cpf.charAt(i + 1);
+
+        // Verificação do primeiro dígito
+        int iterador = 10;
+        int total = 0;
+        for (int posicao = 0; posicao < 9; posicao++) {
+            int resultado = Character.getNumericValue(cpf.charAt(posicao)) * iterador;
+            total += resultado;
+            iterador--;
+        }
+
+        int resto = (total * 10) % 11;
+
+        // Caso o resto seja igual ao 10. dígito, continuar a verificação do segundo dígito;
+        // Caso contrário, retornar false
+        if (resto == Character.getNumericValue(cpf.charAt(9))) {
+            iterador = 11;
+            total = 0;
+            for (int posicao = 0; posicao < 10; posicao++) {
+                int resultado = Character.getNumericValue(cpf.charAt(posicao)) * iterador;
+                total += resultado;
+                iterador--;
+            }
+
+            resto = (total * 10) % 11;
+
+            // Caso o resto seja igual ao 11. dígito, retornar true (CPF válido)
+            // Caso contrário, retornar false
+            if (resto == Character.getNumericValue(cpf.charAt(10))) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
-
-    public static char cpfValido(String cpf) {
-        return cpf.charAt(0);
-    }
-
-    
 }
--- app/src/main/java/com/recursos/validadorcpf/activity_validador.java
+++ app/src/main/java/com/recursos/validadorcpf/activity_validador.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class activity_validador extends AppCompatActivity {
     EditText cpfInput;
@@ -15,7 +17,27 @@ public class activity_validador extends AppCompatActivity {
         setContentView(R.layout.activity_cpf);
 
         cpfInput = (EditText) findViewById(R.id.cpf_edit);
-        TextWatcher mascara = Mascara.mask(cpfInput, "###.###.###-##");
-        cpfInput.addTextChangedListener(mascara);
+        TextWatcher mascara = Mascara.mask(cpfInput, "###.###.###-##");  // Cria a máscara
+        cpfInput.addTextChangedListener(mascara); // Adiciona a máscara ao EditText
+    }
+
+    public void validarCPF(View v) {
+        String cpfPuro = cpfInput.getText().toString(); // cpfPuro: CPF com os dígitos formatadores, conforme máscara
+        if (cpfPuro.equals("")) {
+            Toast.makeText(getApplicationContext(), "Digite um CPF no campo indicado.", Toast.LENGTH_LONG).show();
+        } else {
+            String cpfNumero = Mascara.unmask(cpfPuro); // cpfNumero: String contendo apenas os dígitos do CPF
+
+            boolean valido = Validador.isValid(cpfNumero); // Valida o CPF e retorna boolean
+
+            String texto;
+            if (valido) {
+                texto = cpfPuro + " é um CPF válido!";
+            } else {
+                texto = cpfPuro + " não é um CPF válido.";
+            }
+
+            Toast.makeText(getApplicationContext(), texto, Toast.LENGTH_LONG).show(); // Cria Toast de acordo com o resultado
+        }
     }
 }
